a3b65d3988173808472790e94dcdb8ebc8587fff
Koushik Dhumantarao <dsnkoushik@gmail.com>
2019-10-02 15:02:10 -0700

    Add existing file

--- sql/619. Biggest Single Number.sql
+++ sql/619. Biggest Single Number.sql
@@ -1,23 +1,25 @@
-#Table my_numbers contains many numbers in column num including duplicated ones.
-#Can you write a SQL query to find the biggest number, which only appears once.
-#
-#+---+
-#|num|
-#+---+
-#| 8 |
-#| 8 |
-#| 3 |
-#| 3 |
-#| 1 |
-#| 4 |
-#| 5 |
-#| 6 | 
-#For the sample data above, your query should return the following result:
-#+---+
-#|num|
-#+---+
-#| 6 |
-#
+"""
+Table my_numbers contains many numbers in column num including duplicated ones.
+Can you write a SQL query to find the biggest number, which only appears once.
+
++---+
+|num|
++---+
+| 8 |
+| 8 |
+| 3 |
+| 3 |
+| 1 |
+| 4 |
+| 5 |
+| 6 | 
+For the sample data above, your query should return the following result:
++---+
+|num|
++---+
+| 6 |
+"""
+
 # Write your MySQL query statement below
 SELECT MAX(num) AS num
 FROM 
