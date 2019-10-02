f407e910758dfb72fa99f98d39e21ebe48d96dbe
Koushik Dhumantarao <dsnkoushik@gmail.com>
2019-10-02 10:07:14 -0700

    Add existing file

---
+++ sql/197. Rising Temperature.sql
@@ -0,0 +1,23 @@
+#Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+#
+#+---------+------------------+------------------+
+#| Id(INT) | RecordDate(DATE) | Temperature(INT) |
+#+---------+------------------+------------------+
+#|       1 |       2015-01-01 |               10 |
+#|       2 |       2015-01-02 |               25 |
+#|       3 |       2015-01-03 |               20 |
+#|       4 |       2015-01-04 |               30 |
+#+---------+------------------+------------------+
+#For example, return the following Ids for the above Weather table:
+#
+#+----+
+#| Id |
+#+----+
+#|  2 |
+#|  4 |
+#+----+
+#
+# Write your MySQL query statement below
+SELECT w1.Id
+FROM weather w1, weather w2
+WHERE DATEDIFF(w1.RecordDate, w2.RecordDate) = 1 AND w2.Temperature < w1.Temperature
